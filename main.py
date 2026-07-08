f928de8b67555fa9e02a9e34ed7ca28f5ce9a7c9
syellatrading-max <syellatrading@gmail.com>
2026-07-08 20:43:28 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+
+print("====================================")
+print(" AI XAUUSD SIGNAL BOT")
+print(" Bot berhasil dijalankan")
+print("====================================")
+
+while True:
+    print("Bot sedang memantau harga XAUUSD...")
+    time.sleep(60)
